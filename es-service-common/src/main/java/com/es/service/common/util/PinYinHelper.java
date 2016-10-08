29471be986346f0e56fda12802389c5e76fdd537
wanghailin <hailin1.wang@downjoy.com>
2016-10-08 13:38:54 +0800

    添加多音字支持

--- es-service-common/src/main/java/com/es/service/common/util/PinYinHelper.java
+++ es-service-common/src/main/java/com/es/service/common/util/PinYinHelper.java
@@ -1,5 +1,7 @@
 package com.es.service.common.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,6 +16,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
 
 /**
  * 汉语拼音处理，中文字符转拼音
@@ -178,11 +182,117 @@ public class PinYinHelper {
         return rs.toString();
     }
 
+    /**
+     * 传入要分析的上行词，全拼解析，只解析汉字，英文，数字（包含多音字）
+     * 
+     * @param word
+     * @return
+     */
+    public static List<StringBuilder> getPinYinByMultitone(String word) {
+        char[] ch = word.trim().toCharArray();
+        List<StringBuilder> builds = Lists.newArrayList(new StringBuilder());
+        try {
+            if (ch.length > 40) {
+                builds.add(new StringBuilder(PinyinHelper.toHanyuPinyinString(word, format, " ")));
+                return builds;
+            }
+            // 解析
+            String s_ch;
+            Set<String> yin;
+            for (int i = 0; i < ch.length; i++) {
+                s_ch = Character.toString(ch[i]);
+                yin = new HashSet<String>();
+                if (s_ch.matches("[\u4e00-\u9fa5]+")) {
+                    // 汉字
+                    yin.addAll(Arrays.asList(PinyinHelper.toHanyuPinyinStringArray(ch[i], format)));
+                } else if (s_ch.matches("[\u0030-\u0039]+")) {
+                    // 0-9
+                    yin.add(s_ch);
+                } else if (s_ch.matches("[\u0041-\u005a]+") || s_ch.matches("[\u0061-\u007a]+")) {
+                    // a-zA-Z
+                    yin.add(s_ch);
+                }
+
+                for (int j = 0, len = builds.size(); j < len; j++) {
+                    String temp = builds.get(j).toString();
+                    int ycount = 0;
+                    for (String y : yin) {
+                        if (ycount > 0) {
+                            builds.add(new StringBuilder(temp).append(y));
+                        } else {
+                            builds.get(j).append(y);
+                        }
+                        ycount++;
+                    }
+                }
+
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            e.printStackTrace();
+        }
+        return builds;
+    }
+    
+    /**
+     * 首字母解析，只解析汉字，英文，数字（包含多音字）
+     * 
+     * @param word
+     * @return
+     */
+    public static List<StringBuilder> getPinYinPrefixByMultitone(String word) {
+        char[] ch = word.trim().toCharArray();
+        List<StringBuilder> builds = Lists.newArrayList(new StringBuilder());
+        try {
+            if (ch.length > 40) {
+                builds.add(new StringBuilder(PinyinHelper.toHanyuPinyinString(word, format, " ")));
+                return builds;
+            }
+            // 解析
+            String s_ch;
+            Set<String> yin;
+            for (int i = 0; i < ch.length; i++) {
+                s_ch = Character.toString(ch[i]);
+                yin = new HashSet<String>();
+                if (s_ch.matches("[\u4e00-\u9fa5]+")) {
+                    // 汉字
+                    String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(ch[i], format);
+                    for(String pinyin : pinyinArray){
+                        yin.add(String.valueOf(pinyin.charAt(0)));
+                    }
+                } else if (s_ch.matches("[\u0030-\u0039]+")) {
+                    // 0-9
+                    yin.add(s_ch);
+                } else if (s_ch.matches("[\u0041-\u005a]+") || s_ch.matches("[\u0061-\u007a]+")) {
+                    // a-zA-Z
+                    yin.add(s_ch);
+                }
+                
+                for (int j = 0, len = builds.size(); j < len; j++) {
+                    String temp = builds.get(j).toString();
+                    int ycount = 0;
+                    for (String y : yin) {
+                        if (ycount > 0) {
+                            builds.add(new StringBuilder(temp).append(y));
+                        } else {
+                            builds.get(j).append(y);
+                        }
+                        ycount++;
+                    }
+                }
+                
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            e.printStackTrace();
+        }
+        return builds;
+    }
+
     public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
-//        String s1 = "大话西游";
-//        String s2 = getAnalyzePinYin(s1) + "," + getAnalyzePinYinPrefix(s1);
-//        System.out.println(s1);
-//        System.out.println(s2);
-        System.out.println(getPinYin(" 我 的 世 界"));
+        // String s1 = "大话西游";
+        // String s2 = getAnalyzePinYin(s1) + "," + getAnalyzePinYinPrefix(s1);
+        // System.out.println(s1);
+        // System.out.println(s2);
+        //System.out.println(getPinYinByMultitone("阿调空调"));
+        System.out.println(getPinYinPrefixByMultitone("阿调空调"));
     }
 }
